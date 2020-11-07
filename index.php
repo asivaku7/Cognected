a83a2b57d3a4a3c822a152dc54ee4f3528e8af38
Nate <nate.troksa@gmail.com>
2020-11-07 12:21:58 -0700

    CORS test5

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php include_once("working.html");
-      header('Access-Control-Allow-Origin: *');
-
-      header('Access-Control-Allow-Methods: GET, POST');
-
-      header("Access-Control-Allow-Headers: X-Requested-With");
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
 ?>
