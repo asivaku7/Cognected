485cc76ef0fe0b94a7cd1cd3d5ba7344a1e33b29
Nate <nate.troksa@gmail.com>
2020-11-07 19:18:46 -0700

    CORS test6.4

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php include_once("working.html");
-header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Origin: https://cognected.herokuapp.com/');
 header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
 ?>
